3a815060ac42c7f07b711eb64b91692a145edb7c
Phebe Ren <renxt96@g.ucla.edu>
2019-12-09 17:11:08 +0000

    add add.yaml, requirements.txt, modify settings.py

--- squirrel/squirrel/settings.py
+++ squirrel/squirrel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2(=icjhzt^q0ff+fd(sic6ka!+6thlsikjcib-89ux$=o$e0xh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'collected-static'
